7adf73031dc794de4835de334f375c64d7d1a7cb
KayDjl <sergkadet@yandex.ru>
2024-08-17 21:22:18 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 ﻿from cx_Freeze import setup, Executable
 import os
 
-# Замените "main.py" на имя вашего основного файла
+
 executables = [Executable("alien_invasion.py", base="Win32GUI" )]
 
-# Укажите все необходимые файлы и директории
+
 include_files = [
     "score_info/",    # папка с данными
     "images/",  # папка с изображениями
     "sound/",  # папка со звуками
-    # Добавьте все необходимые папки и файлы
+
 ]
 
 # Список всех используемых модулей
@@ -17,7 +17,7 @@ packages = ["pygame",
             "os",
             "time",
             "sys"
-            ]  # Замените на ваши пакеты
+            ]  
 
 options = {
     'build_exe': {
